00fd4e88c4077a65578c142fd1607d7e04421419
duckupus <75720972+duckupus@users.noreply.github.com>
2023-06-28 11:30:23 +0800

    add splashScreen functionality

---
+++ app/src/main/java/com/sp/studylah/splashScreen.java
@@ -0,0 +1,29 @@
+package com.sp.studylah;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class splashScreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        //create splash screen for 3 seconds before going to main activity
+        Thread thread = new Thread(() -> {
+            try {
+                Thread.sleep(3000);
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+            finally { //always run this
+                //start main activity
+                Intent intent = new Intent(splashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        thread.start();
+    }
+}
