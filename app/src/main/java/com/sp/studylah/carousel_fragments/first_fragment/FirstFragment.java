c405b90a9bb01d142c1a7e8f62fadc76f9ea8600
duckupus <75720972+duckupus@users.noreply.github.com>
2023-07-15 23:12:41 +0800

    proof of concept done!

--- app/src/main/java/com/sp/studylah/carousel_fragments/first_fragment/FirstFragment.java
+++ app/src/main/java/com/sp/studylah/carousel_fragments/first_fragment/FirstFragment.java
@@ -9,15 +9,20 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.sp.studylah.R;
+import com.sp.studylah.RecyclerItemClickListener;
 import com.sp.studylah.databinding.FragmentFirstBinding;
 import com.sp.studylah.view1;
+
+import java.util.Objects;
 
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
     private ConstraintLayout layout;
+    private RecyclerView recyclerView;
 
     @Override
     public View onCreateView(
@@ -40,7 +45,47 @@ public class FirstFragment extends Fragment {
                 startActivity(intent);
             }
         });
+        recyclerView = view.findViewById(R.id.recycler_view_carousel_first_fragment);
+        CarouselDateAdapter adapter = new CarouselDateAdapter();
+        recyclerView.setAdapter(adapter);
+        recyclerView.setRecyclerListener(new RecyclerView.RecyclerListener() {
+            @Override
+            public void onViewRecycled(@NonNull RecyclerView.ViewHolder holder) {
+                ViewGroup.LayoutParams params = ((CarouselDateAdapter.ViewHolder) holder).textView.getLayoutParams();
+                params.height = adapter.height;
+                params.width = adapter.width;
+                ((CarouselDateAdapter.ViewHolder) holder).textView.setLayoutParams(params); //resets back to original size when recycled.
+            }
+        });
+        RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
+        int count = layoutManager.getChildCount();
+        recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(getContext(), recyclerView, new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                CarouselDateAdapter.ViewHolder viewholder = (CarouselDateAdapter.ViewHolder) recyclerView.findViewHolderForAdapterPosition(position);
+                viewholder.textView.setText("CLICKED! " + position);
+                ViewGroup.LayoutParams params = ((CarouselDateAdapter.ViewHolder) Objects.requireNonNull(recyclerView.findViewHolderForAdapterPosition(position))).textView.getLayoutParams();
+                params.height = (int) (adapter.height * 1.2);
+                params.width = (int) (adapter.width * 1.2);
+                ((CarouselDateAdapter.ViewHolder) Objects.requireNonNull(recyclerView.findViewHolderForAdapterPosition(position))).textView.setLayoutParams(params);
+                for(int i = position - recyclerView.getChildCount(); i < position + recyclerView.getChildCount(); i++) {
+                    if(recyclerView.findViewHolderForAdapterPosition(i) == null) { //avoids invalid positions
+                        continue;
+                    }
+                    if(i != position) {
+                        ViewGroup.LayoutParams params2 = ((CarouselDateAdapter.ViewHolder) recyclerView.findViewHolderForAdapterPosition(i)).textView.getLayoutParams();
+                        params2.height = adapter.height;
+                        params2.width = adapter.width;
+                        ((CarouselDateAdapter.ViewHolder) Objects.requireNonNull(recyclerView.findViewHolderForAdapterPosition(i))).textView.setLayoutParams(params2);
+                    }
+                }
+            }
 
+            @Override
+            public void onLongItemClick(View view, int position) {
+
+            }
+        }));
     }
 
     @Override
